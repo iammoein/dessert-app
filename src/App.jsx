e6ffbb378f4a6256834414ea5f22c4663c2e46ad
Moein Moradi <moein.morradi@gmail.com>
2025-11-30 15:39:10 +0330

    init git

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <main className="bg-black w-full h-screen">
+
+    <div className="text-white">
+      this file for testing...
+    </div>
+    </main>
+  );
+}
